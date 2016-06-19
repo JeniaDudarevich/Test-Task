c2ec3b265788b90f2ac4a5571659687af6fa499a
JeniaDudarevich <yauheni_dudarevich@mail.ru>
2016-06-20 03:15:25 +0400

    Test Task Third Version

--- js/own_script.js
+++ js/own_script.js
@@ -1,20 +1,20 @@
 $(init);
 function init() {
-    for (var i = 1; i < 11; i++) {
-        $('#makeMeDraggable' + i).draggable({
-            stop: stopFunc
+    for (var i = 1; i < 10; i++) {
+        $('#block' + i).draggable({
+            stop: handleDragEvent
         });
     }
-    $('#makeMeDroppable').droppable({
+    $('#boundaryBlock').droppable({
         drop: handleDropEvent
     });
 }
 function handleDropEvent(event, ui) {
     var draggable = ui.draggable;
     var id = draggable.attr('id');
-    $('#'+ id).css('display', 'none');
+    $('#'+ id).css('display','none');
 }
-function stopFunc (event, ui) {
+function handleDragEvent (event, ui) {
     var id = event.target.id;
     var checkNone = $('#' + id).css('display');
     if (checkNone !== 'none') {
